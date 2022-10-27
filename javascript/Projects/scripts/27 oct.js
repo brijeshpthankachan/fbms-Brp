9e9d9b0b4af253171c6873d39d80bee5b0cf8085
Yi-Sunshin <brijesh.thankachan@experionglobal.com>
2022-10-27 12:55:26 +0530

    ok

--- javascript/Projects/scripts/27 oct.js
+++ javascript/Projects/scripts/27 oct.js
@@ -1,20 +1,20 @@
 console.log('hi');
 var tag = document.getElementById("one");
 console.log(tag);
-tag.innerText="brijeshpthankachan";
-tag.style.color='red';
+tag.innerText = "brijeshpthankachan";
+tag.style.color = 'red';
 tag.style.fontFamily = 'cursive';
 
 
 // gets the parent element of the element
-console.log(tag.parentElement); 
-tag.parentElement.style.background='ivory';
+console.log(tag.parentElement);
+tag.parentElement.style.background = 'ivory';
 
 
 document.write(' <h1> from javascript </h1> ')
 
 
-document.write( `
+document.write(`
 
 <ul>
 <li>one</li>
@@ -30,4 +30,120 @@ var paragraphs = document.getElementsByTagName('p');
 console.log(paragraphs);
 
 
-paragraphs[0].style.color='green';
+paragraphs[0].style.color = 'green';
+
+
+//  using inneeHTML for manipulating dom makes vulnerabilities and also inefficent
+
+
+for (var i = 0; i < 50; i++) {
+
+    var di = document.getElementById('button');
+    di.innerHTML += ` 
+    
+
+      <button>${i}</button>
+
+
+    `
+}
+
+
+
+var x = document.getElementsByTagName('button');
+
+for (var i = 0; i < x.length; i++) {
+
+    x[i].style.background = `rgb(255, ${i+100} , ${i+20})`;
+    x[i].style.margin = '5px'
+
+}
+
+
+
+
+
+// using nodelist
+// node list allows to use advanced css selectors
+
+var qs = document.querySelectorAll('div div button')
+qs.forEach(e => {
+
+    e.style.background = 'green';
+}
+)
+
+
+
+
+// creating elements from javascipt , dom manipulation  
+
+
+var p = document.createElement('p');
+p.innerText= "Paragraph created by js";
+
+document.getElementById('button').appendChild(p);
+
+
+
+var table = document.createElement('table');
+var raw = document.createElement('tr');
+var col = document.createElement('td');
+
+raw.innerText="raw";
+col.innerText="column";
+
+document.getElementById('table').appendChild(table).appendChild(raw).appendChild(col);
+
+
+
+// e is the event object
+
+function clickevent(e){
+
+    // alert("clicked");
+    e.target.innerText = 'clicked';
+}
+
+
+var yar = document.getElementById('x');
+
+console.log(yar);
+
+
+function hide(){
+
+
+    yar.style.display="none";
+
+}
+
+function show(){
+
+    yar.style.display="block";
+
+
+}
+
+
+document.querySelectorAll('button').forEach(e=>{
+
+    e.addEventListener('click',()=>{
+
+        // console.log("button clicked");
+        e.classList.add('bg-primary');
+        e.classList.remove('txt-white');
+    })
+})
+
+
+
+
+document.querySelectorAll('button').forEach(e=>{
+
+
+        // console.log("button clicked");
+        e.classList.add('bg-primary');
+        e.classList.remove('txt-white');
+    
+})
