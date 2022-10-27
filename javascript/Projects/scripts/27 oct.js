83d534f9f4e2d2ee64bf1ab489559bc2e695f5d1
Yi-Sunshin <brijesh.thankachan@experionglobal.com>
2022-10-27 10:17:22 +0530

    ok

--- javascript/Projects/scripts/27 oct.js
+++ javascript/Projects/scripts/27 oct.js
@@ -4,3 +4,30 @@ console.log(tag);
 tag.innerText="brijeshpthankachan";
 tag.style.color='red';
 tag.style.fontFamily = 'cursive';
+
+
+// gets the parent element of the element
+console.log(tag.parentElement); 
+tag.parentElement.style.background='ivory';
+
+
+document.write(' <h1> from javascript </h1> ')
+
+
+document.write( `
+
+<ul>
+<li>one</li>
+<li>two</li>
+<li>two</li>
+</ul>
+
+` );
+
+
+
+var paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs);
+
+
+paragraphs[0].style.color='green';
