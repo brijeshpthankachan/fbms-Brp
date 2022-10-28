13b1227afeaea054d1b9a834ad6c2793cfcb8362
Yi-Sunshin <brijesh.thankachan@experionglobal.com>
2022-10-28 09:14:22 +0530

    ok

---
+++ Assignments Extra/labworks/calc.js
@@ -0,0 +1,37 @@
+var btn = document.querySelectorAll("button");
+
+console.log(btn);
+
+btn.forEach(e=>{
+
+    e.addEventListener('click',()=>{
+
+        let play = document.getElementById("dis");
+        console.log(play.value)
+
+        if(e.value==='='){
+
+            try {
+                play.innerText=eval(play.value);
+
+            } catch (error) {
+                play.innerHTML="ERROR press clear to continue "
+            
+            }
+            
+        }
+        else if(e.value==='clr'){
+
+            play.innerText="";
+        }
+        else{
+
+            play.innerText=play.value+e.value;
+            console.log(e.value);
+
+        }
+
+
+        // console.log(hi);
+    })
+})
