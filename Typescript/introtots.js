537c77439fcb018c046aa0b3be645c31af15f121
Yi-Sunshin <brijesh.thankachan@experionglobal.com>
2022-11-13 11:01:01 +0530

    ok

---
+++ Typescript/introtots.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "hello brijesh p thankachan";
+greetings = greetings.toUpperCase();
+console.log(greetings);
+var num = 987;
+var isLoggedIn = true;
+console.log(isLoggedIn);
+console.log(num.valueOf());
---
+++ Typescript/introtots.ts
@@ -0,0 +1,15 @@
+let greetings : string = "hello brijesh p thankachan";
+greetings= greetings.toUpperCase();
+console.log(greetings);
+ 
+
+let num :number = 987;
+
+
+let isLoggedIn:boolean = true;
+
+console.log(isLoggedIn);
+console.log(num.valueOf());
+
+
+export{}
